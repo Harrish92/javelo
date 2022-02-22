521f0383e9a91a4f14b427ed060bf7ff40f1f81a
Yoan <>
2022-02-22 09:21:18 +0100

    SwissBounds.java

---
+++ src/ch/epfl/javelo/projection/SwissBounds.java
@@ -0,0 +1,32 @@
+package ch.epfl.javelo.projection;
+
+/**
+ * Préconditions à l'exécution d'un algorithme.
+ *
+ * @author Yoan Giovannini (303934)
+ */
+public final class SwissBounds {
+    public final static double MIN_E = 2485000;
+    public final static double MAX_E = 2834000;
+    public final static double MIN_N = 1075000;
+    public final static double MAX_N = 1296000;
+    public final static double WIDTH = MAX_E - MIN_E;
+    public final static double HEIGHT = MAX_N - MIN_N;
+
+    /**
+     * Vérifie si les coordonnées données en paramètre sont en suisse.
+     *
+     * @param e La coordonnée E.
+     * @param n La coordonnée N.
+     * @return Un booléen vrai si les coordonnées données en paramètre
+     * sont en suisse et faux autrement.
+     */
+    public static boolean containsEN(double e, double n) {
+        if ( e <= MAX_E && e >= MIN_E && n <= MAX_N && n >= MIN_N ) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
