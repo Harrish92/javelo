fdebb8aa338d3bae5039080e77f1ba4324e6052a
Harrishan Raveendran <harrishan.raveendran@epfl.ch>
2022-02-22 09:59:21 +0100

    Math2 terminé

--- src/ch/epfl/javelo/Math2.java
+++ src/ch/epfl/javelo/Math2.java
@@ -25,13 +25,22 @@ public final class Math2{
      *
      * @param y0 valeur de y en x0
      * @param y1 valeur de y en x
-     * @param x
+     * @param x nombre à virgule
      * @return retourne une interpolation de la forme de y = a*x+y0
      */
     static double interpolate(double y0, double y1, double x){
         return Math.fma(y1,x,y0);
     }
 
+    /**
+     *
+     * @param min minimum de l'intervalle
+     * @param v un entier
+     * @param max maximum de l'intervalle
+     * @return retourne la valeur de max, si v est supérieur au bord supérieur de l'intervalle.
+     *         retourn la valeur de min si v est inférieur au bord inférieur de l'intervalle.
+     *         Sinon retourne la valeur de v.
+     */
     static int clamp(int min, int v, int max){
         Preconditions.checkArgument(min > max);
         if(v < min){
@@ -46,6 +55,9 @@ public final class Math2{
 
     }
 
+    /**
+     * méthode similaire à celui au-dessus, mais les paramètres et le type de retour sont des nombre à virgule.
+     */
     static double clamp(double  min, double v, double max){
         Preconditions.checkArgument(min > max);
         if(v < min){
@@ -60,22 +72,57 @@ public final class Math2{
 
     }
 
+    /**
+     *
+     * @param x un nombre à virgule
+     * @return retourne y = asinh(x) en fonction de la valeur de x.
+     */
     static double asinh(double x){
         return Math.log(x+Math.sqrt(1+Math.pow(x,2)));
     }
 
+    /**
+     *
+     * @param uX cordonnée X du point u
+     * @param uY cordonnée Y du point u
+     * @param vX cordonnée X du point v
+     * @param vY cordonnée Y du point v
+     * @return retourne le produit scalaire entre deux vecteurs.
+     */
     static double dotProduct(double uX, double uY, double vX, double vY){
         return Math.fma(uX, vX, uX*uY);
     }
 
+    /**
+     *
+     * @param uX cordonnée X du point u
+     * @param uY cordonnée Y du point u
+     * @return retourne le carré de la norme d'un vecteur.
+     */
     static double squareNorm(double uX, double uY){
         return Math.pow(uX, 2) + Math.pow(uY, 2);
     }
 
+    /**
+     *
+     * @param uX cordonnée X du point u
+     * @param uY cordonnée Y du point u
+     * @return retourne la norme d'un vecteur.
+     */
     static double norm(double uX, double uY){
         return Math.sqrt(squareNorm(uX,uY));
     }
 
+    /**
+     *
+     * @param aX cordonnée X du point A
+     * @param aY cordonnée Y du point A
+     * @param bX cordonnée X du point B
+     * @param bY cordonnée Y du point B
+     * @param pX cordonnée X du point P
+     * @param pY cordonnée Y du point P
+     * @return retourne la longeur de la projection du vecteur AP sur le vecteur AB.
+     */
     static double projectionLength(double aX, double aY, double bX, double bY, double pX, double pY){
         double uX = Math.abs(aX - pX);
         double uY = Math.abs(aY - pY);
