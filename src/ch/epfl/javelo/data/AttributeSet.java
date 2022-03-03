efaf50207c58f4b1d156857b848f55ddd30fd3f3
Yoan <>
2022-03-03 13:53:37 +0100

    AttributeSet ...

--- src/ch/epfl/javelo/data/AttributeSet.java
+++ src/ch/epfl/javelo/data/AttributeSet.java
@@ -1,6 +1,9 @@
 package ch.epfl.javelo.data;
 
+import ch.epfl.javelo.Preconditions;
+
 /**
+ * Représente les attributs sous la forme d'un nombre binaire
  *
  * @author Yoan Giovannini (303934)
  */
@@ -11,18 +14,46 @@ public record AttributeSet(long bits) {
      * @param bits
      */
     public AttributeSet {
-
+        int x = 0;
+        long max = 0L;
+        while(x < Attribute.COUNT){
+            max += Math.pow(2, x);
+            x++;
+        }
+        Preconditions.checkArgument(bits <= max);
     }
 
+    /**
+     * Créé un nouvel AttributeSet à partir d'une liste d'attributs.
+     * @param attributes une liste d'attributs.
+     * @return un objet représentant un ensemble d'attributs.
+     */
     public static AttributeSet of(Attribute... attributes) {
-        return null;
+        long attributeSet = 0L;
+        for(Attribute attribute : attributes) {
+            if((attribute.ordinal() >> attributeSet) % 2 == 0) {
+                attributeSet += 1L << attribute.ordinal();
+                //attributeSet+= Math.pow(2, attribute.ordinal());
+            }
+        }
+        return new AttributeSet(attributeSet);
     }
 
+    /**
+     * Vérifie que cette liste (this) contient l'attribut donné en argument.
+     * @param attribute un attribut.
+     * @return vrai si la liste contient l'attribut, faux autrement.
+     */
     public boolean contains(Attribute attribute) {
         return false;
 
     }
 
+    /**
+     *
+     * @param that
+     * @return
+     */
     public boolean intersects(AttributeSet that) {
         return false;
     }
--- test/ch/epfl/javelo/projection/PointWebMercatorTest.java
+++ test/ch/epfl/javelo/projection/PointWebMercatorTest.java
@@ -1,7 +1,21 @@
 package ch.epfl.javelo.projection;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointWebMercatorTest {
+
+    @Test
+    void errorTest(){
+        assertThrows(IllegalArgumentException.class, () -> new PointWebMercator(0,-0.1));
+        assertThrows(IllegalArgumentException.class, () -> new PointWebMercator(-0.2,0));
+        assertThrows(IllegalArgumentException.class, () -> new PointWebMercator(-0.3,-0.4));
+        assertThrows(IllegalArgumentException.class, () -> new PointWebMercator(1,1.01));
+        assertThrows(IllegalArgumentException.class, () -> new PointWebMercator(1.2,1));
+        assertThrows(IllegalArgumentException.class, () -> new PointWebMercator(2,-0.1));
+    }
+
+
 
 }
