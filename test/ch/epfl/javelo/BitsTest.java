d341aa8402b3bdc7fe468cd0597afd984bc2ee5f
Harrishan Raveendran <harrishan.raveendran@epfl.ch>
2022-03-04 10:33:10 +0100

    Bits finit et testé

--- test/ch/epfl/javelo/BitsTest.java
+++ test/ch/epfl/javelo/BitsTest.java
@@ -8,16 +8,31 @@ public class BitsTest {
     // res 1010111111101011101010111110000
     // 01|01010111111101011101010111110000
 
+
+    @Test
+    void NegLengthTest(){
+        assertThrows(IllegalArgumentException.class, ()->{
+            var b = Bits.extractUnsigned(0b11001010111111101011101010111110, -1, 4);
+        });
+    }
+
+    @Test
+     void UnsignedLength32(){
+        assertThrows(IllegalArgumentException.class, ()->{
+            var b = Bits.extractUnsigned(0b11001010111111101011101010111110, -1, 32);
+        });
+    }
+
     @Test
     void extractSignedTest(){
         var b = Bits.extractSigned(0b11001010111111101011101010111110,8,4); // value en binaire :
-        assertEquals(1, b);
+        assertEquals(-6, b);
     }
 
     @Test
     void extractUnSignedTest(){
         var b = Bits.extractUnsigned(0b11001010111111101011101010111110,8,4);
-        assertEquals(1, b);
+        assertEquals(10, b);
     }
 
 }
