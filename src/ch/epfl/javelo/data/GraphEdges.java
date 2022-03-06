4a76158906078e72d355c8d056c184de06cfa1ca
Harrishan Raveendran <harrishan.raveendran@epfl.ch>
2022-03-06 13:19:18 +0100

    GraphNodes terminé

---
+++ src/ch/epfl/javelo/data/GraphEdges.java
@@ -0,0 +1,43 @@
+package ch.epfl.javelo.data;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.nio.ShortBuffer;
+
+public record GraphEdges(ByteBuffer edgesBuffer, IntBuffer profileIds, ShortBuffer elevations) {
+
+    /**
+     *
+     * @param edgeId arrête identité
+     * @return vrai ssi l'arrête
+     */
+    public boolean isInverted(int edgeId){
+
+    }
+
+    public int targetNodeId(int edgeId){
+
+    }
+
+    public double length(int edgeId){
+
+    }
+
+    public double elevationGain(int edgeId){
+
+    }
+
+    public boolean hasProfile(int edgeId){
+
+    }
+
+    public float[] profileSamples(int edgeId){
+
+    }
+
+    public int attributesIndex(int edgeId){
+
+    }
+
+
+}
