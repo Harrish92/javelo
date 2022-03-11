2c5f9c40100830adf42c0a8d67314eaa242f2d3d
Yoan <>
2022-03-11 13:46:59 +0100

    GraphSectorsTest: 70%

--- test/ch/epfl/javelo/data/GraphSectorsTest.java
+++ test/ch/epfl/javelo/data/GraphSectorsTest.java
@@ -2,17 +2,19 @@ package ch.epfl.javelo.data;
 
 import ch.epfl.javelo.projection.PointCh;
 import ch.epfl.javelo.projection.SwissBounds;
+import ch.epfl.javelo.data.GraphSectors.Sector;
 import org.junit.jupiter.api.Test;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GraphSectorsTest {
 
-    private record Sector(int startNodeId, int endNodeId) {}
+
 
     @Test
     void sectorTest() {
@@ -30,10 +32,11 @@ class GraphSectorsTest {
         liste.add(new Sector(13,113));
         liste.add(new Sector(19071233,19073624));
         int d = 2000;
-        assertArrayEquals(liste.toArray(), s.sectorsInArea(new PointCh(SwissBounds.MIN_E+d, SwissBounds.MIN_N+d), d).toArray());
-
-        //TODO: coord négatives ?
-        //TODO: assertequals pour sector
+        for(int i = 0; i < liste.size(); i++) {
+            assertEquals(liste.get(i), s.sectorsInArea(new PointCh(SwissBounds.MIN_E + d, SwissBounds.MIN_N + d), d).get(i));
+        }
+        assertEquals(liste.get(0), s.sectorsInArea(new PointCh(SwissBounds.MIN_E, SwissBounds.MIN_N), d).get(0));
+        //assertEquals(liste.get(0), s.sectorsInArea(new PointCh(SwissBounds.MAX_E+d, SwissBounds.MAX_N+d), d).get(0));
 
     }
 }
