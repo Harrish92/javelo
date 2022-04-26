6ecb0437ec8c98f8791dd8cea91458aae50a9f38
Harrishan Raveendran <harrishan.raveendran@epfl.ch>
2022-04-26 09:40:58 +0200

    ressources

--- src/ch/epfl/javelo/gui/BaseMapManager.java
+++ src/ch/epfl/javelo/gui/BaseMapManager.java
@@ -1,28 +1,61 @@
-//package ch.epfl.javelo.gui;
+package ch.epfl.javelo.gui;
 
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.input.ZoomEvent;
 import javafx.scene.layout.Pane;
-/*
+
 public final class BaseMapManager {
     private final TileManager tm;
-    private final
+    private final ObjectProperty<MapViewParameters> property;
+    private boolean redrawNeeded;
 
- */
 
-
-/*
-    public BaseMapManager(TileManager tm, WaypointManager wpm, Canvas canvas){
+    public BaseMapManager(TileManager tm /*, WaypointManager wpm */, ObjectProperty<MapViewParameters> property){
+        redrawNeeded = false;
         this.tm = tm;
+        this.property = property;
+        Pane pane = pane();
+        Canvas canvas = new Canvas();
+        assert pane != null;
+        canvas.widthProperty().bind(pane.widthProperty());
+        canvas.getGraphicsContext2D();
+
+        canvas.sceneProperty().addListener((p, oldS, newS) -> {
+            assert oldS == null;
+            newS.addPreLayoutPulseListener(this::redrawIfNeeded);
+        });
 
     }
 
     public Pane pane(){
-        Canvas canvas = new Canvas();
         Pane pane = new Pane();
-        canvas.getGraphicsContext2D().drawImage();
-        canvas.widthProperty().bind(pane.widthProperty());
-        return null;
+        /*
+        pane.setOnScroll(e ->{
+            int zooming =  (int) (e.getDeltaY() + 0.5);
+            int zoomLevel = property.get().zoomLevel() + zooming;
+            property.set(new MapViewParameters(zoomLevel, property.get().CoordX(), property.get().CoordY()));
+        });
+
+         */
+
+        return pane;
+
     }
+
+    private void redrawIfNeeded() {
+        if (!redrawNeeded) return;
+        redrawNeeded = false;
+
+        // … à faire : dessin de la carte
+    }
+
+    private void redrawOnNextPulse() {
+        redrawNeeded = true;
+        Platform.requestNextPulse();
+    }
+
 }
 
- */
