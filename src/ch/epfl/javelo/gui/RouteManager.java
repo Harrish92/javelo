6da68fc7fe8066aa864dad758ac8bf7e87117dea
Harrishan Raveendran <harrishan.raveendran@epfl.ch>
2022-05-06 22:02:32 +0200

    RouteManager terminé, mais RouteBean renvoie NullPointerException

--- src/ch/epfl/javelo/gui/RouteManager.java
+++ src/ch/epfl/javelo/gui/RouteManager.java
@@ -1,10 +1,10 @@
 package ch.epfl.javelo.gui;
 
 
+import ch.epfl.javelo.projection.PointCh;
 import ch.epfl.javelo.projection.PointWebMercator;
 import javafx.beans.property.ObjectProperty;
 import javafx.geometry.Point2D;
-import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polyline;
@@ -18,73 +18,114 @@ public class RouteManager {
     private final ObjectProperty<MapViewParameters> mapViewParametersProperty;
     private final Consumer<String> erreurs;
     private final RouteBean rb;
-    private Polyline polyline;
-    private Circle cercle;
-    private Group group;
+    private final Polyline polyline;
+    private final Circle cercle;
+
 
     public RouteManager(RouteBean rb, ObjectProperty<MapViewParameters> mapViewParametersProperty, Consumer<String> erreurs){
         this.rb = rb;
         this.erreurs = erreurs;
         this.mapViewParametersProperty = mapViewParametersProperty;
         this.pane = new Pane();
-        this.cercle = new Circle();
+        this.cercle = new Circle(5);
+        polyline = new Polyline();
+
+        polyline.setId("route");
+        cercle.setId("highlight");
+
+        pane.getChildren().add(polyline);
+        pane.getChildren().add(cercle);
 
         pane.setPickOnBounds(false);
-        group = new Group();
 
 
         dessiner();
+
 
 
         mapViewParametersProperty.addListener((p, oldS, newS) -> {
             if (oldS.zoomLevel() != newS.zoomLevel()){
                 dessiner();
             }else{
-                glissementSurLaCarte(oldS.topLeft(), newS.topLeft());
+                glissementSurLaCarte();
             }
+
 
         });
 
+        rb.getRouteProperty().addListener((p, oldS, newS) -> {
+            dessiner();
+        });
+
+        cercle.setOnMouseClicked(e->{
+            ajouterUnWayPoint(e.getX(), e.getY());
+        });
+
+
 
     }
 
+    private void ajouterUnWayPoint(double x, double y) {
+        Point2D p2d = new Point2D(x, y).add(mapViewParametersProperty.get().topLeft());
+        Point2D p2dlocaltoparent = cercle.localToParent(p2d);
+        PointWebMercator pwm = PointWebMercator.of(mapViewParametersProperty.get().zoomLevel(),
+                p2dlocaltoparent.getX(), p2dlocaltoparent.getY()) ;
 
-    private void glissementSurLaCarte(Point2D olds, Point2D news) {
+        for(Waypoint wpFromTheList : rb.getPointsList()){
+            int wayPointNewNodeId = rb.getRouteProperty().get().nodeClosestTo(rb.highlightedPosition());
+            if(wpFromTheList.NodeId() != wayPointNewNodeId){
+                Waypoint wp = new Waypoint(pwm.toPointCh(),
+                        rb.getRouteProperty().get().nodeClosestTo(rb.highlightedPosition()));
+                List<Waypoint> wp_list = new ArrayList<>();
+                wp_list.add(wp);
+                rb.setPointsList(wp_list);
+            }else{
+                erreurs.accept("Un point de passage est déjà présent à cet endroit !");
 
-        polyline.setLayoutX(news.getX());
-        polyline.setLayoutY(news.getY());
-        System.out.println(polyline.getLayoutX());
+            }
+        }
+    }
+
+    private void glissementSurLaCarte() {
+        polyline.setLayoutX(-mapViewParametersProperty.get().coordX());
+        polyline.setLayoutY(-mapViewParametersProperty.get().coordY());
+
+        cercle.setLayoutX(-mapViewParametersProperty.get().coordX());
+        cercle.setLayoutY(-mapViewParametersProperty.get().coordY());
+
     }
 
     private void dessiner(){
-        pane.getChildren().removeAll(pane.getChildren());
-        group.getChildren().removeAll(group.getChildren());
-
-        cercle.setRadius(5);
-        cercle.setId("highlight");
-
-        pane.getChildren().add(cercle);
-
 
         List<Double> doubleArrayList = new ArrayList<>();
-        polyline = new Polyline();
 
-        polyline.setId("route");
-
-        for(Waypoint wp : rb.getPointsList()){
-            PointWebMercator pwm = PointWebMercator.ofPointCh(wp.PointCH());
-
-
-            doubleArrayList.add(mapViewParametersProperty.get().viewX(pwm));
-            doubleArrayList.add(mapViewParametersProperty.get().viewY(pwm));
+        for(PointCh r : rb.getRouteProperty().get().points()){
+            PointWebMercator pwm = PointWebMercator.ofPointCh(r);
+            doubleArrayList.add(pwm.xAtZoomLevel(mapViewParametersProperty.get().zoomLevel()));
+            doubleArrayList.add(pwm.yAtZoomLevel(mapViewParametersProperty.get().zoomLevel()));
         }
 
-        polyline.getPoints().setAll(doubleArrayList);
+
         polyline.setVisible(!rb.getPointsList().isEmpty());
+        cercle.setVisible(!rb.getPointsList().isEmpty());
+
+        PointCh PositionChDuCercle = rb.getRouteProperty().get().pointAt(rb.highlightedPosition());
+
+        PointWebMercator pwm = PointWebMercator.ofPointCh(PositionChDuCercle);
+
+        polyline.getPoints().setAll(doubleArrayList);
+
+        polyline.setLayoutX(-mapViewParametersProperty.get().coordX());
+        polyline.setLayoutY(-mapViewParametersProperty.get().coordY());
+
+        cercle.setCenterX(pwm.xAtZoomLevel(mapViewParametersProperty.get().zoomLevel()));
+        cercle.setCenterY(pwm.yAtZoomLevel(mapViewParametersProperty.get().zoomLevel()));
+
+        cercle.setLayoutX(-mapViewParametersProperty.get().coordX());
+        cercle.setLayoutY(-mapViewParametersProperty.get().coordY());
 
 
-        group.getChildren().add(polyline);
-        pane.getChildren().add(group);
+
     }
 
 
