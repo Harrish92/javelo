7ac2c0ae0e2fae9b1c40ae1f22bd9e96cc2a94e6
Yoan <>
2022-05-20 14:44:16 +0200

    JaVelo: 50%

--- src/ch/epfl/javelo/gui/JaVelo.java
+++ src/ch/epfl/javelo/gui/JaVelo.java
@@ -2,7 +2,22 @@ package ch.epfl.javelo.gui;
 
 
 import ch.epfl.javelo.data.Graph;
+import ch.epfl.javelo.routing.CityBikeCF;
+import ch.epfl.javelo.routing.ElevationProfile;
+import ch.epfl.javelo.routing.RouteComputer;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.nio.file.Path;
@@ -18,7 +33,58 @@ public final class JaVelo extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Graph graph = Graph.loadFrom(Path.of("lausanne"));
-        //TODO: compléters
+        Graph graph = Graph.loadFrom(Path.of("javelo-data/ch_west"));
+        Path cacheBasePath = Path.of("osm-cache");
+        String tileServerHost = "tile.openstreetmap.org";
+        TileManager tileManager =
+                new TileManager(cacheBasePath, tileServerHost);
+        BorderPane mainPane = new BorderPane();
+        ObservableList<Waypoint> waypoints = FXCollections.observableArrayList();
+        ReadOnlyDoubleProperty hp = new SimpleDoubleProperty(1000);
+        RouteBean routeBean = new RouteBean(new RouteComputer(graph, new CityBikeCF(graph)));
+        routeBean.setPointsList(waypoints);
+        ErrorManager errorManager = new ErrorManager();
+        AnnotatedMapManager annotatedMapManager = new AnnotatedMapManager(graph,tileManager, routeBean, errorManager :: displayError);
+        SplitPane splitPane = new SplitPane();
+        splitPane.setOrientation(Orientation.VERTICAL);
+        splitPane.getItems().add(annotatedMapManager.pane());
+        mainPane.setCenter(splitPane);
+
+
+
+        //barre de menu
+        MenuItem menuItem = new MenuItem("Exporter GPX");
+        Menu menu = new Menu("Fichier");
+        MenuBar menuBar = new MenuBar();
+        menu.getItems().add(menuItem);
+        menuBar.getMenus().add(menu);
+        mainPane.setTop(menuBar);
+
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(600);
+        primaryStage.setScene(new Scene(mainPane));
+        primaryStage.setTitle("JaVelo");
+        primaryStage.show();
+
+        routeBean.getRouteProperty().addListener( l ->{
+                menuItem.setVisible(routeBean.getRouteProperty().getValue() != null);
+                if(routeBean.getRouteProperty().get() != null){
+                    ElevationProfileManager elevationProfileManager = new ElevationProfileManager(
+                            routeBean.elevationProfile(),routeBean.highlightedPositionProperty());
+                    if(splitPane.getItems().size() < 2) {
+                        splitPane.getItems().add(elevationProfileManager.pane());
+                    }
+                    /*routeBean.highlightedPositionProperty().bindBidirectional((Property<Number>) Bindings.createDoubleBinding(() ->
+                            elevationProfileManager.mousePositionOnProfileProperty().doubleValue(),
+                            elevationProfileManager.mousePositionOnProfileProperty()
+                    ));*/
+                    //routeBean.highlightedPositionProperty().bindBidirectional((Property<Number>) elevationProfileManager.mousePositionOnProfileProperty());
+                }
+                else{
+                    splitPane.getItems().removeAll(splitPane.getItems());
+                    splitPane.getItems().add(annotatedMapManager.pane());
+                }
+        });
+
     }
 }
