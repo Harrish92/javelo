6168e25a516c9bcb04a7a7b739e58a5d2b27c756
Harrishan Raveendran <harrishan.raveendran@epfl.ch>
2022-05-20 14:38:55 +0200

    ErrorManager terminé à 90%

--- src/ch/epfl/javelo/gui/ErrorManager.java
+++ src/ch/epfl/javelo/gui/ErrorManager.java
@@ -1,20 +1,42 @@
 package ch.epfl.javelo.gui;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 public final class ErrorManager {
-    private Pane pane;
+    private final Pane pane;
+    private final VBox vBox;
+    private final Text errorMessage;
+    private SequentialTransition sequentialTransition;
+    private final static double TRANSITION_IN_SECONDS = 0.2;
+    private final static double FROM_VALUE = 0;
+    private final static double TO_VALUE = 0.8;
 
     public ErrorManager(){
-        VBox vBox = new VBox();
         this.pane = new Pane();
-        init();
+        this.vBox = new VBox();
+        vBox.getStyleClass().add("error");
+        this.errorMessage = new Text();
+        vBox.getChildren().add(errorMessage);
+        pane.getChildren().add(vBox);
+
+        initAnimation();
 
     }
 
-    private void init(){
-        VBox vBox = new VBox();
+    private void initAnimation(){
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(TRANSITION_IN_SECONDS), vBox);
+        fadeTransition.setFromValue(FROM_VALUE);
+        fadeTransition.setToValue(TO_VALUE);
+        this.sequentialTransition = new SequentialTransition(new PauseTransition(Duration.seconds(2)),
+                fadeTransition
+        );
+
     }
 
     public Pane pane(){
@@ -22,6 +44,9 @@ public final class ErrorManager {
     }
 
     public void displayError(String message){
-
+        sequentialTransition.stop();
+        errorMessage.setText(message);
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sequentialTransition.play();
     }
 }
