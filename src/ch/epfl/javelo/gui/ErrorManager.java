c1cbcd1e05e1b439fd245d9122b39271a7ee0995
Harrishan Raveendran <harrishan.raveendran@epfl.ch>
2022-05-20 15:38:53 +0200

    ErrorManager terminé à 90%

--- src/ch/epfl/javelo/gui/ErrorManager.java
+++ src/ch/epfl/javelo/gui/ErrorManager.java
@@ -9,44 +9,41 @@ import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 public final class ErrorManager {
-    private final Pane pane;
     private final VBox vBox;
     private final Text errorMessage;
-    private SequentialTransition sequentialTransition;
     private final static double TRANSITION_IN_SECONDS = 0.2;
     private final static double FROM_VALUE = 0;
     private final static double TO_VALUE = 0.8;
+    private SequentialTransition sequentialTransition;
 
     public ErrorManager(){
-        this.pane = new Pane();
         this.vBox = new VBox();
         vBox.getStyleClass().add("error");
         this.errorMessage = new Text();
         vBox.getChildren().add(errorMessage);
-        pane.getChildren().add(vBox);
-
         initAnimation();
 
     }
 
     private void initAnimation(){
+        java.awt.Toolkit.getDefaultToolkit().beep();
         FadeTransition fadeTransition = new FadeTransition(Duration.seconds(TRANSITION_IN_SECONDS), vBox);
         fadeTransition.setFromValue(FROM_VALUE);
         fadeTransition.setToValue(TO_VALUE);
-        this.sequentialTransition = new SequentialTransition(new PauseTransition(Duration.seconds(2)),
+        sequentialTransition = new SequentialTransition(new PauseTransition(Duration.seconds(2)),
                 fadeTransition
         );
-
     }
 
     public Pane pane(){
-        return pane;
+        return vBox;
     }
 
     public void displayError(String message){
+        System.out.println(1);
         sequentialTransition.stop();
         errorMessage.setText(message);
-        java.awt.Toolkit.getDefaultToolkit().beep();
+
         sequentialTransition.play();
     }
 }
